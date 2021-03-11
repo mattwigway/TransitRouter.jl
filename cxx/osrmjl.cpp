74ece8a022a7f18be9f8492273291dc200306ba9
Matthew Wigginton Conway <matt@indicatrix.org>
2021-03-10 23:54:30 -0500

    add C++ interface to OSRM

---
+++ cxx/osrmjl.cpp
@@ -0,0 +1,70 @@
+#include <osrm/table_parameters.hpp>
+#include <osrm/engine_config.hpp>
+#include <osrm/coordinate.hpp>
+#include <osrm/status.hpp>
+#include <osrm/json_container.hpp>
+#include <osrm/osrm.hpp>
+
+#include <cstdlib>
+
+// set up wrapper functions so we can use ccall in julia to call out to cpp osrm
+// see https://isocpp.org/wiki/faq/mixing-c-and-cpp#overview-mixing-langs
+
+extern "C" struct osrm::OSRM * init_osrm (char * osrm_path, char * algorithm) {
+    using namespace osrm;
+    EngineConfig config;
+    config.storage_config = {osrm_path};
+    config.use_shared_memory = false;  // TODO this may have something to do with thread safety
+
+    if (strcmp(algorithm, "ch") == 0) config.algorithm = EngineConfig::Algorithm::CH;
+    else if (strcmp(algorithm, "mld") == 0) config.algorithm = EngineConfig::Algorithm::MLD;
+    else throw std::runtime_error("algorithm must be 'ch' or 'mld'");
+
+    static osrm::OSRM engn{config};
+
+    return &engn;
+}
+
+extern "C" void distance_matrix(struct osrm::OSRM * osrm, size_t n_origins, double * origin_lats, double * origin_lons,
+    size_t n_destinations, double * destination_lats, double * destination_lons, double * durations, double * distances) {
+    using namespace osrm;
+
+    // Create table parameters. concatenate origins and destinations into coordinates, set origin/destination references.
+    TableParameters params;
+    for (size_t i = 0; i < n_origins; i++) {
+        params.sources.push_back(i);
+        params.coordinates.push_back({util::FloatLongitude{origin_lons[i]}, util::FloatLatitude{origin_lats[i]}});
+    }
+
+    for (size_t i = 0; i < n_destinations; i++) {
+        params.destinations.push_back(i + n_origins);
+        params.coordinates.push_back({util::FloatLongitude{destination_lons[i]}, util::FloatLatitude{destination_lats[i]}});
+    }
+
+    params.annotations = TableParameters::AnnotationsType::All;
+
+    engine::api::ResultT result = json::Object();    
+
+    Status stat = osrm->Table(params, result);
+
+    if (stat != Status::Ok) throw std::runtime_error("osrm failed!");
+
+    auto &json_result = result.get<json::Object>();
+
+    std::vector<json::Value> jdurations = json_result.values["durations"].get<json::Array>().values;
+    std::vector<json::Value> jdistances = json_result.values["distances"].get<json::Array>().values;
+
+    // copy it into the result array
+    // jdurations, jdistances are multidimensional arrays
+
+    for (size_t destination = 0; destination < n_destinations; destination++) {
+        for (size_t origin = 0; origin < n_origins; origin++) {
+            // julia arrays: col-major order
+            size_t off = destination * n_origins + origin;
+            durations[off] = double(jdurations.at(origin).get<json::Array>().values.at(destination).get<json::Number>().value);
+            distances[off] = double(jdistances.at(origin).get<json::Array>().values.at(destination).get<json::Number>().value);
+        }
+    }
+}
+
+// todo shutdown osrm! memory leak!
---
+++ src/osrm/osrm.jl
@@ -0,0 +1,45 @@
+# functions to call out to OSRM
+
+module OSRM
+
+
+struct Coordinate
+    latitude::Float64
+    longitude::Float64
+end
+
+const osrmjl = "libosrmjl"
+
+function start_osrm(file_path::String, algorithm::String)::Ptr{Any}
+    return @ccall osrmjl.init_osrm(file_path::Cstring, algorithm::Cstring)::Ptr{Any}
+end
+
+function distance_matrix(osrm::Ptr{Any}, origins::Vector{Coordinate}, destinations::Vector{Coordinate})
+    n_origins::Csize_t = length(origins)
+    n_destinations::Csize_t = length(destinations)
+    origin_lats::Vector{Float64} = map(c -> c.latitude, origins)
+    origin_lons::Vector{Float64} = map(c -> c.longitude, origins)
+    destination_lats::Vector{Float64} = map(c -> c.latitude, destinations)
+    destination_lons::Vector{Float64} = map(c -> c.longitude, destinations)
+
+    durations::Array{Float64, 2} = fill(-1.0, (n_origins, n_destinations))::Array{Float64, 2}
+    distances::Array{Float64, 2} = fill(-1.0, (n_origins, n_destinations))::Array{Float64, 2}
+
+    @ccall osrmjl.distance_matrix(
+        osrm::Ptr{Any},
+        n_origins::Csize_t,
+        origin_lats::Ptr{Float64},
+        origin_lons::Ptr{Float64},
+        n_destinations::Csize_t,
+        destination_lats::Ptr{Float64},
+        destination_lons::Ptr{Float64},
+        durations::Ptr{Float64},
+        distances::Ptr{Float64}
+    )::Cvoid
+
+    return (durations=durations, distances=distances)
+end
+
+export start_osrm, distance_matrix, Coordinate
+
+end
