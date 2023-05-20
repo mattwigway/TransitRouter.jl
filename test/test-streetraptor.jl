a6e29861ee35e5b3a926f22fcd5a94b2f1ca667c
Matt Bhagat-Conway <matt@indicatrix.org>
2023-05-20 17:43:57 -0400

    custom comparator to ensure changes were due to pickup/dropoff types

--- test/test-streetraptor.jl
+++ test/test-streetraptor.jl
@@ -6,6 +6,111 @@
     @struct_isequal TransitRouter.StreetRaptorResult
     @struct_isequal TransitRouter.AccessEgress
 
+    # we now respect pickup type and dropoff type, which we didn't when the snapshots were originally created
+    # This affects three stops in the SBMTD network: stop_ids 685, 679, and 671. The only differences in the times should
+    # be at these three stops, or downstream from them
+    function predecessor_stops(res, stop, transfer, round)
+        if transfer
+            res.times_at_stops_each_round[round, stop] != MT || error("round $round, stop $stop not reached")
+            # step back if reached in earlier round
+            if round > 1 && res.times_at_stops_each_round[round, stop] == res.times_at_stops_each_round[round - 1, stop]
+                return predecessor_stops(res, stop, transfer, round - 1)
+            end
+            prev_stop = res.transfer_prev_stop[round, stop]
+            if prev_stop == TransitRouter.INT_MISSING
+                prev_stop = stop
+            end
+        else
+            if round > 1
+                if round > 1 && res.non_transfer_times_at_stops_each_round[round, stop] == res.non_transfer_times_at_stops_each_round[round - 1, stop]
+                    return predecessor_stops(res, stop, transfer, round - 1)
+                end
+
+                res.prev_stop[round, stop] != IM || error("round $round, stop $stop not reached")
+
+                prev_stop = res.prev_stop[round, stop]
+                round -= 1
+            else
+                return (stop,)
+            end
+        end
+
+        return (stop, predecessor_stops(res, prev_stop, !transfer, round)...)
+    end
+
+    predecessor_stops_contain(res, stop, predecessors, transfer, round) = any(predecessors .∈ Ref(predecessor_stops(res, stop, transfer, round)))
+
+    const predecessors = [535, 529, 522]
+
+    function Base.isequal(sr1::TransitRouter.StreetRaptorResult, sr2::TransitRouter.StreetRaptorResult)
+        if !isequal(sr1.times_at_destinations_each_departure_time, sr2.times_at_destinations_each_departure_time)
+            return false
+        end
+
+        if !isequal(sr1.egress_stop_each_departure_time, sr2.egress_stop_each_departure_time)
+            return false
+        end
+        if !isequal(sr1.access_geometries, sr2.access_geometries)
+            return false
+        end
+        if !isequal(sr1.egress_geometries, sr2.egress_geometries)
+            return false
+        end
+        if !isequal(sr1.departure_date_time, sr2.departure_date_time)
+            return false
+        end
+
+        for (minute, val) in enumerate(zip(sr1.raptor_results, sr2.raptor_results))
+            rr1, rr2 = val
+            for (round, stop) in Tuple.(findall(rr1.times_at_stops_each_round .≠ rr2.times_at_stops_each_round))
+                if !predecessor_stops_contain(rr2, stop, predecessors, true, round)
+                    @error "Mismatch at minute $minute, round $round, stop $stop, transfer"
+                    return false
+
+                end
+            end
+
+            for (round, stop) in Tuple.(findall(rr1.non_transfer_times_at_stops_each_round .≠ rr2.non_transfer_times_at_stops_each_round))
+                if !predecessor_stops_contain(rr2, stop, predecessors, false, round)
+                    @error "Mismatch at minute $minute, round $round, stop $stop, non-transfer"
+                    return false
+                end
+            end
+
+            for (round, stop) in Tuple.(findall(rr1.prev_trip .≠ rr2.prev_trip))
+                if !predecessor_stops_contain(rr2, stop, predecessors, false, round)
+                    @error "Mismatch at minute $minute, round $round, stop $stop, non-transfer"
+                    return false
+                end
+            end
+
+            for (round, stop) in Tuple.(findall(rr1.prev_stop .≠ rr2.prev_stop))
+                if !predecessor_stops_contain(rr2, stop, predecessors, false, round)
+                    @error "Mismatch at minute $minute, round $round, stop $stop, non-transfer"
+                    return false
+                end
+            end
+
+            for (round, stop) in Tuple.(findall(rr1.prev_boardtime .≠ rr2.prev_boardtime))
+                if !predecessor_stops_contain(rr2, stop, predecessors, false, round)
+                    @error "Mismatch at minute $minute, round $round, stop $stop, non-transfer"
+                    return false
+                end
+            end
+
+            for (round, stop) in Tuple.(findall(rr1.transfer_prev_stop .≠ rr2.transfer_prev_stop))
+                if !predecessor_stops_contain(rr2, stop, predecessors, true, round)
+                    @error "Mismatch at minute $minute, round $round, stop $stop, transfer"
+                    return false
+                end
+            end
+        end
+
+        return true
+    end
+
+
+
     # tests for streetraptor
     # don't run if OSRM binaries not available (currently not available on CI)
     if !success(`which osrm-extract`)
