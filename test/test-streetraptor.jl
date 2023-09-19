3b8264574711b5033b85345e4210dd300bec550e
Matt Bhagat-Conway <matt@indicatrix.org>
2023-09-19 08:41:56 -0400

    add custom comparator to ensure new results only differ from snapshots by not having spurious trips

--- test/test-streetraptor.jl
+++ test/test-streetraptor.jl
@@ -6,6 +6,35 @@
     @struct_isequal TransitRouter.AccessEgress
     @struct_isequal TransitRouter.Leg
 
+    import Dates: DateTime
+
+    const EARLIEST_DEPARTURE = DateTime(2023, 5, 10, 6, 0)
+    const LATEST_ARRIVAL = DateTime(2023, 5, 10, 8, 0)
+
+    function filter_out_early_trips(result)
+        map(result) do dest
+            filter(dest) do trip
+                first(trip).start_time >= EARLIEST_DEPARTURE && last(trip).end_time <= LATEST_ARRIVAL
+            end
+        end
+    end
+
+    # MASSIVE hack - if we define our custom comparator here, snapshot tests can't find it
+    # So we override isapprox in base, since it is a binary function already defined that sorta
+    # matches what we're doing.
+    ResultType = Vector{Vector{Vector{TransitRouter.Leg}}}
+    function Base.isapprox(r1::ResultType, r2::ResultType)
+        fr1 = filter_out_early_trips(r1)
+        fr2 = filter_out_early_trips(r2)
+        @info fr1
+        @info fr2
+        return isequal(fr1, fr2)
+    end
+
+    function Base.show(io::IO, l::TransitRouter.Leg)
+        println(io, "$(l.type != TransitRouter.access ? "  " : "")$(l.type) leg from $(ismissing(l.origin_stop) ? "missing" : l.origin_stop.stop_id)@$(l.start_time) to $(ismissing(l.destination_stop) ? "missing" : l.destination_stop.stop_id)@$(l.end_time) via route $(ismissing(l.route) ? "none" : l.route.route_short_name) ($(l.distance_meters)m)")
+    end
+
     # tests for streetraptor
     # don't run if OSRM binaries not available (currently not available on CI)
     if !success(`which osrm-extract`)
@@ -53,14 +82,14 @@
             # We also test reverse routing here. We do the same trip as the first two above, but make sure that we find a route that arrives
             # at the destination before the requested departure time.
             revres = street_raptor(net, osrm, osrm, LatLon(34.4128, -119.8487), [LatLon(34.4224, -119.7032), LatLon(34.4226, -119.6777)], DateTime(2023, 5, 10, 8, 0);
-                reverse_search=true, max_reverse_search_duration=-7200)
-            @snapshot_test "reverse_street_raptor" revres
+                reverse_search=true, max_reverse_search_duration=7200)
+            @snapshot_test "reverse_street_raptor" revres (isapprox)
 
             # make sure it doesn't ride forever 'neath the streets of Boston if there's no trip to be found (i.e. the departure minute just keeps getting earlier)
             # now, the second destinations is not accessible by transit
             revres2 = street_raptor(net, osrm, osrm, LatLon(34.4128, -119.8487), [LatLon(34.4224, -119.7032), LatLon(34.4561, -119.6819)], DateTime(2023, 5, 10, 8, 0), reverse_search=true,
-                max_reverse_search_duration=-7200)
-            @snapshot_test "reverse_no_path" revres2
+                max_reverse_search_duration=7200)
+            @snapshot_test "reverse_no_path" revres2 (isapprox)
 
             # @test revres2.times_at_destinations_each_departure_time[1, 1] ≤ gt(8, 0)
             # @test size(revres2.times_at_destinations_each_departure_time, 1) == 121 # search should have been cut off after 7200 seconds
@@ -68,7 +97,7 @@
 
             # if it rolls back to the previous day (negative times), make sure trace does not fail
             revres3 = street_raptor(net, osrm, osrm, LatLon(34.4128, -119.8487), [LatLon(34.4224, -119.7032), LatLon(34.4226, -119.6777)], DateTime(2023, 5, 10, 0, 5), reverse_search=true)
-            @snapshot_test "reverse_rollback" revres3
+            @snapshot_test "reverse_rollback" revres3 (isapprox)
         end
     end
 end
